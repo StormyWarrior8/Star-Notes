f317a6189d861b7fcea114500b97ffd288fe992b
Brian Hines <brian@projectweekend.net>
2014-08-23 16:53:08 -0500

    Add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function( grunt ) {
+
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( "package.json" ),
+        watch: {
+            scripts: {
+                files: [
+                    "app/*"
+                ],
+                tasks: [ "concat:scripts" ]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                banner: "/*! <%= pkg.name %> <%= grunt.template.today( 'yyyy-mm-dd' ) %> */",
+                sourceMap: true,
+                sourceMapName: "app/build/project.js.map"
+            },
+            project: {
+                files: {
+                    "app/build/project.js": [
+                        "app/js/*"
+                    ]
+                }
+            }
+        },
+        concat: {
+            scripts: {
+                src: [
+                    "app/js/*"
+                ],
+                dest: "app/build/project.js"
+            },
+            angular: {
+                src: [
+                    "app/bower_components/angular/angular.min.js",
+                    "app/bower_components/angular-route/angular-route.min.js"
+                ],
+                dest: "app/build/angular.js"
+            },
+            jquery: {
+                src: [
+                    "app/bower_components/jquery/dist/jquery.min.js"
+                ],
+                dest: "app/build/jquery.js"
+            },
+            bootstrap: {
+                src: [
+                    "app/bower_components/bootstrap/dist/js/bootstrap.min.js"
+                ],
+                dest: "app/build/bootstrap.js"
+            }
+        }
+    } );
+
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
+    grunt.loadNpmTasks( "grunt-contrib-concat" );
+
+    grunt.registerTask( "default", [
+        "concat:scripts",
+        "concat:jquery",
+        "concat:bootstrap",
+        "concat:angular"
+    ] );
+
+    grunt.registerTask( "deploy",  [
+        "uglify",
+        "concat:angular"
+    ] );
+
+};
