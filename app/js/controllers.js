8629ff904e5299f97f9e39028fc54b8df2851996
Brian Hines <brian@projectweekend.net>
2014-08-23 19:03:26 -0500

    Add main controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,8 @@
 var ctlMod = angular.module( "linksApp.controllers", [] );
 
+ctlMod.controller( "Main", [ "$scope", function ( $scope ) {
+
+} ] );
 
 ctlMod.controller( "Home", [ "$scope", function ( $scope ) {
 
