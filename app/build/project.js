3f78f18c59992be7125b900ed9b27e31355256e8
Brian Hines <brian@projectweekend.net>
2014-08-24 10:48:20 -0500

    Add is active highlighting function for nav

--- app/build/project.js
+++ app/build/project.js
@@ -24,9 +24,17 @@ angMod.config( [
 
 var ctlMod = angular.module( "linksApp.controllers", [] );
 
-ctlMod.controller( "Main", [ "$scope", function ( $scope ) {
+ctlMod.controller( "Main", [ "$scope", "$location",
+    function ( $scope, $location ) {
 
-} ] );
+        $scope.isActiveNavItem = function ( view ) {
+
+            return view === $location.path();
+
+        };
+
+    } ] );
+
 
 ctlMod.controller( "AddLink", [ "$scope", function ( $scope ) {
 
